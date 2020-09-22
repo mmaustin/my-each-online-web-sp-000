16b972e1e0adeb0f3193bf2ea43b74010f7637bc
mccray austin <tubach75@hotmail.com>
2020-09-22 14:39:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  if block_given?
+    i = 0
+
+    while i < words.length
+      yield words[i]
+      i += 1
+    end
+  else
+    puts "This block should not run!"
+  end
+  words
+end
+my_each(["hi", "hello", "bye", "goodbye"]) do |word|
+  puts word
 end
